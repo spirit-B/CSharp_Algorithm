1ec3a2580203390380cace0bb8191e142952f4e8
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-04-16 09:39:49 +0900

    [level 0] Title: 개미 군단, Time: 0.19 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        answer += hp / 5;
+        hp %= 5;
+        
+        answer += hp / 3;
+        hp %= 3;
+        
+        answer += hp / 1;
+        hp %= 1;
+        
+        return answer;
+    }
+}
