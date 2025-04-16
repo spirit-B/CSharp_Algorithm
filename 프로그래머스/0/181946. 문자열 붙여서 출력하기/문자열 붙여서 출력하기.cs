0462b94bbffa5ac70579d52ef3e7bd8102587740
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-04-16 09:37:20 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 18.73 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] input = Console.ReadLine().Split();
+        Console.WriteLine(string.Join("", input));
+    }
+}
