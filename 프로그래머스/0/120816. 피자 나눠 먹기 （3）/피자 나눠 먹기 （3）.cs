587c739e6f370199df976f7d72fa9d98bf8bfcd9
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-04-17 10:20:03 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.24 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int slice, int n) {
+        int answer = 1;
+        while (true)
+        {
+            if (slice * answer / n == 0)
+            {
+                answer++;
+            }
+            else break;
+        }
+        return answer;
+    }
+}
