a01e6f04fcccaee8d81fef0399fa4b905d1fd43c
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-04-17 22:56:37 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int[] solution(string[] strlist) {
+        int[] answer = new int[strlist.Length];
+        
+        for (int i = 0; i < answer.Length; i++)
+        {
+            answer[i] = strlist[i].Length;
+        }
+        return answer;
+    }
+}
