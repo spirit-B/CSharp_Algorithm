f1145bb468b8101be39701e88cf0ef522a29f9f9
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-04-18 09:34:35 +0900

    [level 0] Title: 문자열 돌리기, Time: 16.82 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        for (int i = 0; i < s.Length; i++)
+        {
+            Console.WriteLine(s[i]);
+        }
+    }
+}
