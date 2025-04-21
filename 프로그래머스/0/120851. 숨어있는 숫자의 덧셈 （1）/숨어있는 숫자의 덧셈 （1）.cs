49081939831cbf2c026d525bc3820540923925e4
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-04-21 09:58:09 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 2.81 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(string my_string) {
+        int answer = 0;
+        // foreach (char c in my_string)
+        // {
+        //     if (char.IsDigit(c)) answer += int.Parse(c.ToString());
+        // }
+        my_string.Where(char.IsDigit).Select(c => answer += int.Parse(c.ToString())).ToArray();
+        return answer;
+    }
+}
