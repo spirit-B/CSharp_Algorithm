b71b39acfe962d6c4e33188fa86ff34769c9d15f
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-05-11 17:18:28 +0900

    [level 0] Title: 조건 문자열, Time: 0.35 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.cs
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cs
@@ -2,28 +2,11 @@ using System;
 
 public class Solution {
     public int solution(string ineq, string eq, int n, int m) {
-        if (ineq == "<")
-        {
-            if (eq == "=")
-            {
-                return n <= m ? 1 : 0;
-            }
-            else
-            {
-                return n < m ? 1 : 0;
-            }
-        }
-        else
-        {
-            if (eq == "=")
-            {
-                return n >= m ? 1 : 0;
-            }
-            else
-            {
-                return n > m ? 1 : 0;
-            }
-        }
+        string ie = ineq + eq;
+        if (ie == ">=") return n >= m ? 1 : 0;
+        else if (ie == "<=") return n <= m ? 1 : 0;
+        else if (ie == ">!") return n > m ? 1 : 0;
+        else if (ie == "<!") return n < m ? 1 : 0;
         return 0;
     }
 }
