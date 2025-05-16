87482f05abb86bf9b853cc3f0f60ab1719325b39
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-05-16 20:33:27 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 6.51 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cs
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cs
@@ -3,13 +3,8 @@ using System.Linq;
 
 public class Solution {
     public int[] solution(int[] arr, int k) {
-        if (k % 2 == 0)
-        {
-            return arr.Select(x => x + k).ToArray();
-        }
-        else
-        {
-            return arr.Select(x => x * k).ToArray();
-        }
+        return k % 2 == 0 ?
+            arr.Select(x => x + k).ToArray()
+            : arr.Select(x => x * k).ToArray();
     }
 }
