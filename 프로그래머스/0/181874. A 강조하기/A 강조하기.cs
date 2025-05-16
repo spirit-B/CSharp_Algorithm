dd9cc2be2eda1570e6bd9713687b5f1ac20b0528
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-05-16 20:25:26 +0900

    [level 0] Title: A 강조하기, Time: 0.69 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.cs
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cs
@@ -1,16 +1,7 @@
 using System;
-using System.Text.RegularExpressions;
 
 public class Solution {
     public string solution(string myString) {
-        string answer = "";
-        for (int i = 0; i < myString.Length; i++)
-        {
-            if (myString[i] == 'a' || myString[i] == 'A')
-                answer += myString[i].ToString().ToUpper();
-            else
-                answer += myString[i].ToString().ToLower();
-        }
-        return answer;
+        return myString.ToLower().Replace('a', 'A');
     }
 }
