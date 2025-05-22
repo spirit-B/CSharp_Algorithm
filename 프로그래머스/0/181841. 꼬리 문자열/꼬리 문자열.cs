344ce8dfff25700bd07ae743b1378620cd84ffc3
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-05-22 09:28:54 +0900

    [level 0] Title: 꼬리 문자열, Time: 1.64 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cs
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cs
@@ -1,14 +1,16 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public string solution(string[] str_list, string ex) {
-        string answer = "";
-        for (int i = 0; i < str_list.Length; i++)
-        {
-            if (str_list[i].Contains(ex)) continue;
-            answer += str_list[i];
-        }
+//         string answer = "";
+//         for (int i = 0; i < str_list.Length; i++)
+//         {
+//             if (str_list[i].Contains(ex)) continue;
+//             answer += str_list[i];
+//         }
         
-        return answer;
+//         return answer;
+        return string.Join("", str_list.Where(s => !s.Contains(ex)).ToArray());
     }
 }
