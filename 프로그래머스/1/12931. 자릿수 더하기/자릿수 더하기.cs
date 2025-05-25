fcddd3d420f84660029d6aed4206cd1bbd53c257
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-05-25 20:04:41 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.43 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -4,16 +4,16 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        // string nums = n.ToString();
-        // foreach(char num in nums)
-        // {
-        //     answer += int.Parse(num.ToString());
-        // }
-        while (n > 0)
+        string nums = n.ToString();
+        foreach(char num in nums)
         {
-            answer += n % 10;
-            n /= 10;
+            answer += int.Parse(num.ToString());
         }
+        // while (n > 0)
+        // {
+        //     answer += n % 10;
+        //     n /= 10;
+        // }
         
         return answer;
     }
