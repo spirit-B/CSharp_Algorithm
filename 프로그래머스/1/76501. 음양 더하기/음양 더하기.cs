c64c687f323061313d14242165e03102481a5cac
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-06-05 11:06:50 +0900

    [level 1] Title: 음양 더하기, Time: 1.61 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
@@ -1,14 +1,16 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(int[] absolutes, bool[] signs) {
-        int answer = 0;
+//         int answer = 0;
         
-        for (int i = 0; i < signs.Length; i++)
-        {
-            if (signs[i] == true) answer += absolutes[i];
-            else answer -= absolutes[i];
-        }
-        return answer;
+//         for (int i = 0; i < signs.Length; i++)
+//         {
+//             if (signs[i] == true) answer += absolutes[i];
+//             else answer -= absolutes[i];
+//         }
+//         return answer;
+        return absolutes.Select((n, i) => signs[i] ? n : -n).Sum();
     }
 }
