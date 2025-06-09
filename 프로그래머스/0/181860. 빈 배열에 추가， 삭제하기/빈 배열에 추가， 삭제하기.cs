fdbca91e87138dd49c2c2c5d262d57ebe37ae28d
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-06-09 09:56:45 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 3.15 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] arr, bool[] flag) {
+        Stack<int> answer = new Stack<int>();
+        for (int i = 0; i < flag.Length; i++)
+        {
+            if (flag[i] == true)
+            {
+                for (int j = 0; j < arr[i] * 2; j++)
+                {
+                    answer.Push(arr[i]);
+                }
+            }
+            else
+            {
+                for (int k = 0; k < arr[i]; k++)
+                {
+                    answer.Pop();
+                }
+            }
+        }
+        return answer.Reverse().ToArray();
+    }
+}
