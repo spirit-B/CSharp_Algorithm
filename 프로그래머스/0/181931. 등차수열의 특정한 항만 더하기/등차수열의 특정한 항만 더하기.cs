728fd2f15eddc1b30423672ff9a729860cf79f90
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-06-11 16:18:32 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.cs
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.cs
@@ -3,6 +3,12 @@ using System.Linq;
 
 public class Solution {
     public int solution(int a, int d, bool[] included) {
-        return included.Select((x, i) => x ? a + d * i : 0).Sum();
+        // return included.Select((x, i) => x ? a + d * i : 0).Sum();
+        int answer = 0;
+        for (int i = 0; i < included.Length; i++)
+        {
+            if (included[i]) answer += a + d * i;
+        }
+        return answer;
     }
 }
