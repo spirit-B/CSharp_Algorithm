4fa64fac897ffc01c8e13cee8fc1459083b5d1c1
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-06-12 10:06:36 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 4.17 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.cs
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Linq;
 
 public class Solution {
     public int[] solution(int[] arr, int divisor) {
         int[] answer = new int[] {};
-        answer = arr.Where(n => n % divisor == 0).ToArray();
-        Array.Sort(answer);
+        answer = arr.Where(n => n % divisor == 0).OrderBy(x => x).ToArray();
         
         if (answer.Length == 0) return new int[] { -1 };
         else return answer;
