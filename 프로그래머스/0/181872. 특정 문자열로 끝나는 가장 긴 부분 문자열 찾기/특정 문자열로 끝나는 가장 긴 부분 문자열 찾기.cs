a34d6c0354342ed8d23fde1d1569f4000c0a3d87
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-06-23 21:37:41 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 7.88 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.cs
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.cs
@@ -2,17 +2,19 @@ using System;
 
 public class Solution {
     public string solution(string myString, string pat) {
-        string answer = "";
+//         string answer = "";
         
-        for (int i = 0; i <= myString.Length; i++)
-        {
-            string newStr = myString.Substring(0, i);
-            if (newStr.EndsWith(pat) && newStr.Length > answer.Length)
-            {
-                answer = newStr;
-            }
-        }
+//         for (int i = 0; i <= myString.Length; i++)
+//         {
+//             string newStr = myString.Substring(0, i);
+//             if (newStr.EndsWith(pat) && newStr.Length > answer.Length)
+//             {
+//                 answer = newStr;
+//             }
+//         }
         
-        return answer;
+//         return answer;
+        Console.WriteLine(myString.LastIndexOf(pat));
+        return myString.Substring(0, myString.LastIndexOf(pat) + pat.Length);
     }
 }
