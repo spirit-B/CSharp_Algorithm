16bdbd9b8886a030f8ec0a2fdfc53f4af02ac1f9
JangHoon Ban <79096544+spirit-B@users.noreply.github.com>
2025-06-25 00:08:14 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.20 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -1,6 +1,9 @@
 public class Solution {
     public string solution(string phone_number) {
-        int maskedCount = phone_number.Length - 4;
-        return new string('*', maskedCount) + phone_number.Substring(maskedCount);
+        // int maskedCount = phone_number.Length - 4;
+        // return new string('*', maskedCount) + phone_number.Substring(maskedCount);
+        string answer = phone_number.Substring(phone_number.Length - 4);
+        answer = answer.PadLeft(phone_number.Length, '*');
+        return answer;
     }
 }
